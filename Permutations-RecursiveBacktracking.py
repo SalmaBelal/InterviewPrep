fc7346412bc6a7255d6823eac7044018e099fedd
Salma Belal <100041807+SalmaBelal@users.noreply.github.com>
2023-03-12 22:15:17 +0000

    Update Permutations-RecursiveBacktracking.py

--- Permutations-RecursiveBacktracking.py
+++ Permutations-RecursiveBacktracking.py
@@ -39,3 +39,45 @@ def permute(string):
     print(all_permutations)
     
 permute("abc")
+
+
+#not using string...list of lists
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        permutation = []
+        result = []
+        
+        def helper(arr):
+            if len(arr) == 0:
+                result.append(permutation.copy())
+
+            for i in range(len(arr)):
+                permutation.append(arr[i])
+                helper(arr[:i] + arr[i+1:])
+                permutation.pop()
+
+        helper(nums)
+
+        return result
+    
+#another way of not using string instead list of lists
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        if len(nums) == 1 :
+            return [nums[:]]
+
+        result = []
+        for i in range(len(nums)): 
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+
+            result.extend(perms)
+            nums.append(n) 
+
+        return result
+
